a27103c05f910eb2e378387f1620c4d46da724ec
erri120 <erri120@users.noreply.github.com>
2019-08-29 11:38:29 +0200

    Formatted the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,103 +5,129 @@ const run = require('gulp-run');
 const change = require('gulp-change');
 const rename = require('gulp-rename');
 const del = require('del');
-const {series} = gulp;
+const { series } = gulp;
 
 let dark = false;
 
-function setDark(){
-    //returns promise so gulp won't complain
-    return new Promise(function(resolve,reject){
-        dark = true;
-        resolve();
-    });
+function setDark() {
+  //returns promise so gulp won't complain
+  return new Promise(function(resolve, reject) {
+    dark = true;
+    resolve();
+  });
 }
-function setLight(){
-    //returns promise so gulp won't complain
-    return new Promise(function(resolve,reject){
-        dark = false;
-        resolve();
-    });
-}
-
-function cleanWorkspace(cb){
-    if(dark){
-        return del('guideDark.pdf');
-    }else{
-        return del('guideLight.pdf');
-    }
+function setLight() {
+  //returns promise so gulp won't complain
+  return new Promise(function(resolve, reject) {
+    dark = false;
+    resolve();
+  });
 }
 
-function optimizeImages(cb){
-    //using gulp-imagemin to compress images, will not compress image that already has been
-    return gulp.src('src/res/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('src/res/images'));
+function cleanWorkspace(cb) {
+  if (dark) {
+    return del('guideDark.pdf');
+  } else {
+    return del('guideLight.pdf');
+  }
 }
 
-function backup(cb){
-    return gulp.src('src/guide.tex')
-        .pipe(gulp.dest('backup'));
+function optimizeImages(cb) {
+  //using gulp-imagemin to compress images, will not compress image that already has been
+  return gulp
+    .src('src/res/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('src/res/images'));
 }
 
-function loadBackup(cb){
-    return gulp.src('backup/guide.tex')
-        .pipe(gulp.dest('src'));
+function backup(cb) {
+  return gulp.src('src/guide.tex').pipe(gulp.dest('backup'));
 }
 
-function packagePdf(cb){
-    //creates the final output file including both the pdf file and the LICENSE file
-    if(dark){
-        return gulp.src('guideDark.pdf')
-        .pipe(gulp.src('LICENSE'))
-        .pipe(zip('guideDarkPdf.zip'))
-        .pipe(gulp.dest('dist'))
-    }else{
-        return gulp.src('guideLight.pdf')
-        .pipe(gulp.src('LICENSE'))
-        .pipe(zip('guideLightPdf.zip'))
-        .pipe(gulp.dest('dist'))
-    }
+function loadBackup(cb) {
+  return gulp.src('backup/guide.tex').pipe(gulp.dest('src'));
 }
 
-function packageImages(cb){
-    return gulp.src('src/res/images/*')
-        .pipe(zip('guideImages.zip'))
-        .pipe(gulp.dest('dist'))
+function packagePdf(cb) {
+  //creates the final output file including both the pdf file and the LICENSE file
+  if (dark) {
+    return gulp
+      .src('guideDark.pdf')
+      .pipe(gulp.src('LICENSE'))
+      .pipe(zip('guideDarkPdf.zip'))
+      .pipe(gulp.dest('dist'));
+  } else {
+    return gulp
+      .src('guideLight.pdf')
+      .pipe(gulp.src('LICENSE'))
+      .pipe(zip('guideLightPdf.zip'))
+      .pipe(gulp.dest('dist'));
+  }
 }
 
-function replaceStr(content){
-    let str = dark ? '%$DARK:' : '%$LIGHT:';
-    str = str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
-    return content.replace(new RegExp(str,'g'),'');
+function packageImages(cb) {
+  return gulp
+    .src('src/res/images/*')
+    .pipe(zip('guideImages.zip'))
+    .pipe(gulp.dest('dist'));
 }
 
-function makeDarkTex(cb){
-    //creates the guideDark.tex file using the guide.tex file and uncommenting all DARK marked lines
-    return gulp.src('src/guide.tex')
-        .pipe(change(replaceStr))
-        .pipe(rename('guideDark.tex'))
-        .pipe(gulp.dest('src/'));
+function replaceStr(content) {
+  let str = dark ? '%$DARK:' : '%$LIGHT:';
+  str = str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1');
+  return content.replace(new RegExp(str, 'g'), '');
 }
 
-function makeLightTex(cb){
-    //creates the guideLight.tex file using the guide.tex file and uncommenting all LIGHT marked lines
-    return gulp.src('src/guide.tex')
-        .pipe(change(replaceStr))
-        .pipe(rename('guideLight.tex'))
-        .pipe(gulp.dest('src/'));    
+function makeDarkTex(cb) {
+  //creates the guideDark.tex file using the guide.tex file and uncommenting all DARK marked lines
+  return gulp
+    .src('src/guide.tex')
+    .pipe(change(replaceStr))
+    .pipe(rename('guideDark.tex'))
+    .pipe(gulp.dest('src/'));
 }
 
-function buildGuide(cb){
-    //using gulp-run and xelatex from your PATH, creates pdf file from either Light or Dark tex file
-    if(dark){
-        return run('xelatex -interaction=nonstopmode -file-line-error -aux-directory=src -include-directory=src/res/sections/ src/guideDark.tex').exec();
-    }else{
-        return run('xelatex -interaction=nonstopmode -file-line-error -aux-directory=src -include-directory=src/res/sections/ src/guideLight.tex').exec();
-    }
+function makeLightTex(cb) {
+  //creates the guideLight.tex file using the guide.tex file and uncommenting all LIGHT marked lines
+  return gulp
+    .src('src/guide.tex')
+    .pipe(change(replaceStr))
+    .pipe(rename('guideLight.tex'))
+    .pipe(gulp.dest('src/'));
+}
+
+function buildGuide(cb) {
+  //using gulp-run and xelatex from your PATH, creates pdf file from either Light or Dark tex file
+  if (dark) {
+    return run(
+      'xelatex -interaction=nonstopmode -file-line-error -aux-directory=src -include-directory=src/res/sections/ src/guideDark.tex'
+    ).exec();
+  } else {
+    return run(
+      'xelatex -interaction=nonstopmode -file-line-error -aux-directory=src -include-directory=src/res/sections/ src/guideLight.tex'
+    ).exec();
+  }
 }
 
 //all commands available using gulp <command>, requires gulp cli
-exports.buildDark = series(setDark,backup,optimizeImages,makeDarkTex,buildGuide,loadBackup,packagePdf,cleanWorkspace);
-exports.buildLight = series(setLight,backup,optimizeImages,makeLightTex,buildGuide,loadBackup,packagePdf,cleanWorkspace);
-exports.packageImages = series(optimizeImages,packageImages);
+exports.buildDark = series(
+  setDark,
+  backup,
+  optimizeImages,
+  makeDarkTex,
+  buildGuide,
+  loadBackup,
+  packagePdf,
+  cleanWorkspace
+);
+exports.buildLight = series(
+  setLight,
+  backup,
+  optimizeImages,
+  makeLightTex,
+  buildGuide,
+  loadBackup,
+  packagePdf,
+  cleanWorkspace
+);
+exports.packageImages = series(optimizeImages, packageImages);
